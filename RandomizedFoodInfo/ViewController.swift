7557724c901e143c4a0bd9bec12314b2df05ad70
giustoe82 <marcog.swe@gmail.com>
2019-07-19 17:59:55 +0200

    Initial Commit

---
+++ RandomizedFoodInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomizedFoodInfo
+//
+//  Created by Marco Giustozzi on 2019-07-19.
+//  Copyright © 2019 marcog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
